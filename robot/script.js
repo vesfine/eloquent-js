54d426f42069ad2a0261123d68925a9c38e21fe0
Artyom <vesfine@yandex.ru>
2021-12-01 11:21:35 +0300

    Adding functionality: finding a path by a robot

--- robot/script.js
+++ robot/script.js
@@ -98,3 +98,28 @@ function routeRobot(state, memory) {
     }
     return {direction: memory[0], memory: memory.slice(1)};
 }
+
+function findRoute(graph, from, to) {
+    let work = [{at: from, route: []}];
+    for (let i = 0; i < work.length; i++) {
+        let {at, route} = work[i];
+        for (let place of graph[at]) {
+            if (place == to) return route.concat(place);
+            if (!work.some(w => w.at == place)) {
+                work.push({at: place, route: route.concat(place)});
+            }
+        }
+    }
+}
+
+function goalOrientedRobot({place, parcels}, route) {
+    if (route.length == 0) {
+        let parcel = parcels[0];
+        if (parcel.place != place) {
+            route = findRoute(roadGraph, place, parcel.place);
+        } else {
+            route = findRoute(roadGraph, place, parcel.address);
+        }
+    }
+    return {direction: route[0], memory: route.slice(1)};
+}
