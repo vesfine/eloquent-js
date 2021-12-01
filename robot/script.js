6515e1996718afba08c624999ceda8b2168db958
Artyom <vesfine@yandex.ru>
2021-12-01 10:30:47 +0300

    Simulation of parcel delivery

--- robot/script.js
+++ robot/script.js
@@ -54,3 +54,40 @@ let next = first.move("Дом Алисы");
 console.log(next.place);
 console.log(next.parcels);
 console.log(first.place);
+
+function runRobot(state, robot, memory) {
+    for (let turn = 0;; turn++) {
+        if (state.parcels.length == 0) {
+            console.log(`Выполнено за ${turn} ходов`);
+            break;
+        }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Переход в направлении ${action.direction}`);
+ }
+}
+
+function randomPick(array) {
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
+}
+
+function randomRobot(state) {
+    return {direction: randomPick(roadGraph[state.place])};
+}
+
+VillageState.random = function(parcelCount = 5) {
+    let parcels = [];
+    for (let i = 0; i < parcelCount; i++) {
+        let address = randomPick(Object.keys(roadGraph));
+        let place;
+        do {
+            place = randomPick(Object.keys(roadGraph));
+        } while (place == address);
+        parcels.push({place, address});
+    }
+    return new VillageState("Почта", parcels);
+};
+
+runRobot(VillageState.random(), randomRobot);
