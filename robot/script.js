362e100c11f6e12d3667182038eb14b05fe8e008
Artyom <vesfine@yandex.ru>
2021-12-01 10:06:27 +0300

    Convert array to data structure

---
+++ robot/script.js
@@ -0,0 +1,27 @@
+const roads = [
+    "Дом Алисы-Дом Боба", "Дом Алисы-Склад",
+    "Дом Алисы-Почта", "Дом Боба-Ратуша",
+    "Дом Дарии-Дом Эрни", "Дом Дарии-Ратуша",
+    "Дом Эрни-Дом Греты", "Дом Греты-Ферма",
+    "Дом Греты-Магазин", "Рынок-Ферама",
+    "Рынок-Почта", "Рынок-Магазин",
+    "Рынок-Ратуша", "Магазин-Ратуша"
+];
+
+function buildGraph(edges) {
+    let graph = Object.create(null);
+    function addEdge(from, to) {
+        if (graph[from] == null) {
+            graph[from] = [to];
+        } else {
+            graph[from].push(to);
+        }
+    }
+    for (let [from, to] of edges.map(r => r.split("-"))) {
+        addEdge(from, to);
+        addEdge(to, from);
+    }
+    return graph;
+}
+
+const roadGraph = buildGraph(roads);
