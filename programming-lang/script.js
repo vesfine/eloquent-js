d273d19d46fcc16bb4ffd8716fa2d7e442c54e34
Artyom <vesfine@yandex.ru>
2021-12-04 19:30:58 +0300

    Complete: functions

--- programming-lang/script.js
+++ programming-lang/script.js
@@ -140,3 +140,39 @@ do(define(total, 0),
             define(count, +(count, 1)))),
    print(total))
    `);
+
+specialForms.fun = (args, scope) => {
+    if (!args.length) {
+        throw new SyntaxError("У функции должно быть тело");
+    }
+    let body = args[args.length - 1];
+    let params = args.slice(0, args.length - 1).map(expr => {
+        if (expr.type != "word") {
+            throw new SyntaxError("Именами параметров должны быть слова");
+        }
+        return expr.name;
+    });
+    return function() {
+        if (arguments.length != params.length) {
+            throw new TypeError("Некорректное число аргументов");
+        }
+        let localScope = Object.create(scope);
+        for (let i = 0; i < arguments.length; i++) {
+            localScope[params[i]] = arguments[i];
+        }
+        return evaluate(body, localScope);
+    };
+};
+
+run(`
+do(define(plusOne, fun(a, +(a, 1))),
+   print(plusOne(10)))
+`);
+
+run(`
+do(define(pow, fun(base, exp,
+    if(==(exp, 0),
+      1,
+      *(base, pow(base, -(exp, 1)))))),
+      print(pow(2, 10)))
+`);
