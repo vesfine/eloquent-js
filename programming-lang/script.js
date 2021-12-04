98217f8d83dc1f7aa0e111178db5908425bc779e
Artyom <vesfine@yandex.ru>
2021-12-04 19:23:21 +0300

    Complete: environment

--- programming-lang/script.js
+++ programming-lang/script.js
@@ -113,3 +113,30 @@ specialForms.define = (args, scope) => {
     scope[args[0].name] = value;
     return value;
 };
+
+const topScope = Object.create(null);
+
+topScope.true = true;
+topScope.false = false;
+
+for (let op of ["+", "-", "*", "/", "==", "<", ">"]) {
+    topScope[op] = Function("a, b", `return a ${op} b;`);
+}
+
+topScope.print = value => {
+    console.log(value);
+    return value;
+}
+
+function run(program) {
+    return evaluate(parse(program), Object.create(topScope));
+}
+
+run(`
+do(define(total, 0),
+   define(count, 1),
+   while(<(count, 11),
+         do(define(total, +(total, count)),
+            define(count, +(count, 1)))),
+   print(total))
+   `);
